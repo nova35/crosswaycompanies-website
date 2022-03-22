750fdc2f36a38a8a9cee0949571959253c34c53f
Antony Owaga <antonyowaga@Antonys-MacBook-Pro.local>
2022-03-22 05:35:55 +0300

    Fixing reloading error

--- .next/server/pages/_app.js
+++ .next/server/pages/_app.js
@@ -1,514 +1,166 @@
-module.exports =
-/******/ (function(modules) { // webpackBootstrap
-/******/ 	// The module cache
-/******/ 	var installedModules = require('../ssr-module-cache.js');
-/******/
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/
-/******/ 		// Check if module is in cache
-/******/ 		if(installedModules[moduleId]) {
-/******/ 			return installedModules[moduleId].exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = installedModules[moduleId] = {
-/******/ 			i: moduleId,
-/******/ 			l: false,
-/******/ 			exports: {}
-/******/ 		};
-/******/
-/******/ 		// Execute the module function
-/******/ 		var threw = true;
-/******/ 		try {
-/******/ 			modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-/******/ 			threw = false;
-/******/ 		} finally {
-/******/ 			if(threw) delete installedModules[moduleId];
-/******/ 		}
-/******/
-/******/ 		// Flag the module as loaded
-/******/ 		module.l = true;
-/******/
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/
-/******/
-/******/ 	// expose the modules object (__webpack_modules__)
-/******/ 	__webpack_require__.m = modules;
-/******/
-/******/ 	// expose the module cache
-/******/ 	__webpack_require__.c = installedModules;
-/******/
-/******/ 	// define getter function for harmony exports
-/******/ 	__webpack_require__.d = function(exports, name, getter) {
-/******/ 		if(!__webpack_require__.o(exports, name)) {
-/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
-/******/ 		}
-/******/ 	};
-/******/
-/******/ 	// define __esModule on exports
-/******/ 	__webpack_require__.r = function(exports) {
-/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 		}
-/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 	};
-/******/
-/******/ 	// create a fake namespace object
-/******/ 	// mode & 1: value is a module id, require it
-/******/ 	// mode & 2: merge all properties of value into the ns
-/******/ 	// mode & 4: return value when already ns object
-/******/ 	// mode & 8|1: behave like require
-/******/ 	__webpack_require__.t = function(value, mode) {
-/******/ 		if(mode & 1) value = __webpack_require__(value);
-/******/ 		if(mode & 8) return value;
-/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
-/******/ 		var ns = Object.create(null);
-/******/ 		__webpack_require__.r(ns);
-/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
-/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
-/******/ 		return ns;
-/******/ 	};
-/******/
-/******/ 	// getDefaultExport function for compatibility with non-harmony modules
-/******/ 	__webpack_require__.n = function(module) {
-/******/ 		var getter = module && module.__esModule ?
-/******/ 			function getDefault() { return module['default']; } :
-/******/ 			function getModuleExports() { return module; };
-/******/ 		__webpack_require__.d(getter, 'a', getter);
-/******/ 		return getter;
-/******/ 	};
-/******/
-/******/ 	// Object.prototype.hasOwnProperty.call
-/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
-/******/
-/******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "";
-/******/
-/******/
-/******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 1);
-/******/ })
-/************************************************************************/
-/******/ ({
-
-/***/ 1:
-/***/ (function(module, exports, __webpack_require__) {
-
-module.exports = __webpack_require__("1TCz");
-
-
-/***/ }),
-
-/***/ "1TCz":
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
 "use strict";
+(() => {
+var exports = {};
+exports.id = 2888;
+exports.ids = [2888];
+exports.modules = {
+
+/***/ 271:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
 // ESM COMPAT FLAG
 __webpack_require__.r(__webpack_exports__);
 
 // EXPORTS
-__webpack_require__.d(__webpack_exports__, "default", function() { return /* binding */ _app_MyApp; });
+__webpack_require__.d(__webpack_exports__, {
+  "default": () => (/* binding */ MyApp)
+});
 
 // EXTERNAL MODULE: external "react/jsx-runtime"
-var jsx_runtime_ = __webpack_require__("F5FC");
-
-// EXTERNAL MODULE: ./public/css/bootstrap.min.css
-var bootstrap_min = __webpack_require__("PVTQ");
-
-// EXTERNAL MODULE: ./node_modules/animate.css/animate.css
-var animate = __webpack_require__("d+3G");
-
-// EXTERNAL MODULE: ./public/css/icofont.min.css
-var icofont_min = __webpack_require__("qkZ4");
-
-// EXTERNAL MODULE: ./public/css/pe-icon-7-stroke.css
-var pe_icon_7_stroke = __webpack_require__("3J8d");
-
-// EXTERNAL MODULE: ./node_modules/react-modal-video/css/modal-video.min.css
-var modal_video_min = __webpack_require__("HP2+");
-
-// EXTERNAL MODULE: ./node_modules/react-accessible-accordion/dist/fancy-example.css
-var fancy_example = __webpack_require__("QYuT");
-
-// EXTERNAL MODULE: ./node_modules/react-image-lightbox/style.css
-var style = __webpack_require__("Y9L4");
-
-// EXTERNAL MODULE: ./public/css/style.scss
-var css_style = __webpack_require__("VoYZ");
-
-// EXTERNAL MODULE: ./public/css/responsive.css
-var responsive = __webpack_require__("B5sv");
-
+var jsx_runtime_ = __webpack_require__(997);
 // EXTERNAL MODULE: ./node_modules/next/app.js
-var app = __webpack_require__("8Bbg");
-var app_default = /*#__PURE__*/__webpack_require__.n(app);
-
-// EXTERNAL MODULE: external "next/head"
-var head_ = __webpack_require__("xnum");
-var head_default = /*#__PURE__*/__webpack_require__.n(head_);
-
+var app = __webpack_require__(7544);
+;// CONCATENATED MODULE: external "next/head"
+const head_namespaceObject = require("next/head");
+var head_default = /*#__PURE__*/__webpack_require__.n(head_namespaceObject);
 // EXTERNAL MODULE: external "react"
-var external_react_ = __webpack_require__("cDcd");
-
-// CONCATENATED MODULE: ./components/Shared/GoTop.js
-
+var external_react_ = __webpack_require__(6689);
+;// CONCATENATED MODULE: ./components/Shared/GoTop.js
 
 
-class GoTop_GoTop extends external_react_["Component"] {
-  constructor(props) {
-    super(props);
-    this.state = {
-      is_visible: false
+class GoTop extends external_react_.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            is_visible: false
+        };
+    }
+    componentDidMount() {
+        var scrollComponent = this;
+        document.addEventListener("scroll", function(e) {
+            scrollComponent.toggleVisibility();
+        });
+    }
+    toggleVisibility() {
+        if (window.pageYOffset > 300) {
+            this.setState({
+                is_visible: true
+            });
+        } else {
+            this.setState({
+                is_visible: false
+            });
+        }
+    }
+    scrollToTop() {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    }
+    render() {
+        const { is_visible  } = this.state;
+        return(/*#__PURE__*/ jsx_runtime_.jsx(jsx_runtime_.Fragment, {
+            children: /*#__PURE__*/ jsx_runtime_.jsx("div", {
+                className: "scroll-to-top",
+                children: is_visible && /*#__PURE__*/ jsx_runtime_.jsx("i", {
+                    className: "pe-7s-angle-up",
+                    onClick: ()=>this.scrollToTop()
+                })
+            })
+        }));
+    }
+};
+
+;// CONCATENATED MODULE: ./pages/_app.js
+
+
+
+
+
+
+
+
+// import '../public/css/style.css';
+
+
+
+
+// import Loader from '../components/Shared/Loader';
+
+class MyApp extends app["default"] {
+    // Preloader
+    state = {
+        loading: true
     };
-  }
-
-  componentDidMount() {
-    var scrollComponent = this;
-    document.addEventListener("scroll", function (e) {
-      scrollComponent.toggleVisibility();
-    });
-  }
-
-  toggleVisibility() {
-    if (window.pageYOffset > 300) {
-      this.setState({
-        is_visible: true
-      });
-    } else {
-      this.setState({
-        is_visible: false
-      });
+    componentDidMount() {
+        this.timerHandle = setTimeout(()=>this.setState({
+                loading: false
+            })
+        , 2000);
     }
-  }
-
-  scrollToTop() {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth"
-    });
-  }
-
-  render() {
-    const {
-      is_visible
-    } = this.state;
-    return /*#__PURE__*/Object(jsx_runtime_["jsx"])(jsx_runtime_["Fragment"], {
-      children: /*#__PURE__*/Object(jsx_runtime_["jsx"])("div", {
-        className: "scroll-to-top",
-        children: is_visible && /*#__PURE__*/Object(jsx_runtime_["jsx"])("i", {
-          className: "pe-7s-angle-up",
-          onClick: () => this.scrollToTop()
-        })
-      })
-    });
-  }
-
-}
-// CONCATENATED MODULE: ./pages/_app.js
-
-
-
-
-function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
-
-function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
-
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-
-
-
-
-
-
-
- // import '../public/css/style.css';
-
-
-
-
- // import Loader from '../components/Shared/Loader';
-
-
-class _app_MyApp extends app_default.a {
-  constructor(...args) {
-    super(...args);
-
-    _defineProperty(this, "state", {
-      loading: true
-    });
-  }
-
-  componentDidMount() {
-    this.timerHandle = setTimeout(() => this.setState({
-      loading: false
-    }), 2000);
-  }
-
-  componentWillUnmount() {
-    if (this.timerHandle) {
-      clearTimeout(this.timerHandle);
-      this.timerHandle = 0;
+    componentWillUnmount() {
+        if (this.timerHandle) {
+            clearTimeout(this.timerHandle);
+            this.timerHandle = 0;
+        }
     }
-  }
-
-  render() {
-    const {
-      Component,
-      pageProps
-    } = this.props;
-    return /*#__PURE__*/Object(jsx_runtime_["jsxs"])(jsx_runtime_["Fragment"], {
-      children: [/*#__PURE__*/Object(jsx_runtime_["jsxs"])(head_default.a, {
-        children: [/*#__PURE__*/Object(jsx_runtime_["jsx"])("meta", {
-          name: "viewport",
-          content: "width=device-width, initial-scale=1, shrink-to-fit=no"
-        }), /*#__PURE__*/Object(jsx_runtime_["jsx"])("title", {
-          children: "Crossway Companies"
-        })]
-      }), /*#__PURE__*/Object(jsx_runtime_["jsx"])(Component, _objectSpread({}, pageProps)), /*#__PURE__*/Object(jsx_runtime_["jsx"])(GoTop_GoTop, {})]
-    });
-  }
-
-}
-
-/***/ }),
-
-/***/ "3J8d":
-/***/ (function(module, exports) {
-
-
-
-/***/ }),
-
-/***/ "8Bbg":
-/***/ (function(module, exports, __webpack_require__) {
-
-module.exports = __webpack_require__("B5Ud")
-
-
-/***/ }),
-
-/***/ "AroE":
-/***/ (function(module, exports) {
-
-function _interopRequireDefault(obj) {
-  return obj && obj.__esModule ? obj : {
-    "default": obj
-  };
-}
-
-module.exports = _interopRequireDefault;
-
-/***/ }),
-
-/***/ "B5Ud":
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
-var _interopRequireDefault = __webpack_require__("AroE");
-
-exports.__esModule = true;
-exports.Container = Container;
-exports.createUrl = createUrl;
-exports.default = void 0;
-
-var _react = _interopRequireDefault(__webpack_require__("cDcd"));
-
-var _utils = __webpack_require__("kYf9");
-
-exports.AppInitialProps = _utils.AppInitialProps;
-exports.NextWebVitalsMetric = _utils.NextWebVitalsMetric;
-/**
-* `App` component is used for initialize of pages. It allows for overwriting and full control of the `page` initialization.
-* This allows for keeping state between navigation, custom error handling, injecting additional data.
-*/
-
-async function appGetInitialProps({
-  Component,
-  ctx
-}) {
-  const pageProps = await (0, _utils.loadGetInitialProps)(Component, ctx);
-  return {
-    pageProps
-  };
-}
-
-class App extends _react.default.Component {
-  // Kept here for backwards compatibility.
-  // When someone ended App they could call `super.componentDidCatch`.
-  // @deprecated This method is no longer needed. Errors are caught at the top level
-  componentDidCatch(error, _errorInfo) {
-    throw error;
-  }
-
-  render() {
-    const {
-      router,
-      Component,
-      pageProps,
-      __N_SSG,
-      __N_SSP
-    } = this.props;
-    return /*#__PURE__*/_react.default.createElement(Component, Object.assign({}, pageProps, // we don't add the legacy URL prop if it's using non-legacy
-    // methods like getStaticProps and getServerSideProps
-    !(__N_SSG || __N_SSP) ? {
-      url: createUrl(router)
-    } : {}));
-  }
-
-}
-
-exports.default = App;
-App.origGetInitialProps = appGetInitialProps;
-App.getInitialProps = appGetInitialProps;
-let warnContainer;
-let warnUrl;
-
-if (false) {} // @deprecated noop for now until removal
-
-
-function Container(p) {
-  if (false) {}
-  return p.children;
-}
-
-function createUrl(router) {
-  // This is to make sure we don't references the router object at call time
-  const {
-    pathname,
-    asPath,
-    query
-  } = router;
-  return {
-    get query() {
-      if (false) {}
-      return query;
-    },
-
-    get pathname() {
-      if (false) {}
-      return pathname;
-    },
-
-    get asPath() {
-      if (false) {}
-      return asPath;
-    },
-
-    back: () => {
-      if (false) {}
-      router.back();
-    },
-    push: (url, as) => {
-      if (false) {}
-      return router.push(url, as);
-    },
-    pushTo: (href, as) => {
-      if (false) {}
-      const pushRoute = as ? href : '';
-      const pushUrl = as || href;
-      return router.push(pushRoute, pushUrl);
-    },
-    replace: (url, as) => {
-      if (false) {}
-      return router.replace(url, as);
-    },
-    replaceTo: (href, as) => {
-      if (false) {}
-      const replaceRoute = as ? href : '';
-      const replaceUrl = as || href;
-      return router.replace(replaceRoute, replaceUrl);
+    render() {
+        const { Component , pageProps  } = this.props;
+        return(/*#__PURE__*/ (0,jsx_runtime_.jsxs)(jsx_runtime_.Fragment, {
+            children: [
+                /*#__PURE__*/ (0,jsx_runtime_.jsxs)((head_default()), {
+                    children: [
+                        /*#__PURE__*/ jsx_runtime_.jsx("meta", {
+                            name: "viewport",
+                            content: "width=device-width, initial-scale=1, shrink-to-fit=no"
+                        }),
+                        /*#__PURE__*/ jsx_runtime_.jsx("title", {
+                            children: "Crossway Companies"
+                        })
+                    ]
+                }),
+                /*#__PURE__*/ jsx_runtime_.jsx(Component, {
+                    ...pageProps
+                }),
+                /*#__PURE__*/ jsx_runtime_.jsx(GoTop, {})
+            ]
+        }));
     }
-  };
-}
-
-/***/ }),
-
-/***/ "B5sv":
-/***/ (function(module, exports) {
-
+};
 
 
 /***/ }),
 
-/***/ "F5FC":
-/***/ (function(module, exports) {
+/***/ 9232:
+/***/ ((module) => {
 
-module.exports = require("react/jsx-runtime");
-
-/***/ }),
-
-/***/ "HP2+":
-/***/ (function(module, exports) {
-
-
+module.exports = require("next/dist/shared/lib/utils.js");
 
 /***/ }),
 
-/***/ "PVTQ":
-/***/ (function(module, exports) {
-
-
-
-/***/ }),
-
-/***/ "QYuT":
-/***/ (function(module, exports) {
-
-
-
-/***/ }),
-
-/***/ "VoYZ":
-/***/ (function(module, exports) {
-
-
-
-/***/ }),
-
-/***/ "Y9L4":
-/***/ (function(module, exports) {
-
-
-
-/***/ }),
-
-/***/ "cDcd":
-/***/ (function(module, exports) {
+/***/ 6689:
+/***/ ((module) => {
 
 module.exports = require("react");
 
 /***/ }),
 
-/***/ "d+3G":
-/***/ (function(module, exports) {
+/***/ 997:
+/***/ ((module) => {
 
-
-
-/***/ }),
-
-/***/ "kYf9":
-/***/ (function(module, exports) {
-
-module.exports = require("next/dist/next-server/lib/utils.js");
-
-/***/ }),
-
-/***/ "qkZ4":
-/***/ (function(module, exports) {
-
-
-
-/***/ }),
-
-/***/ "xnum":
-/***/ (function(module, exports) {
-
-module.exports = require("next/head");
+module.exports = require("react/jsx-runtime");
 
 /***/ })
 
-/******/ });
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = __webpack_require__.X(0, [7544], () => (__webpack_exec__(271)));
+module.exports = __webpack_exports__;
+
+})();
